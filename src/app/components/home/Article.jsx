9044d11bf75a1ce53926adcc0d55b0cd98b892e8
Arjodh Singh <arjodh_singh@Arjodhs-MacBook-Air.local>
2023-05-05 09:51:32 +1000

    demo version 2.2

--- src/app/components/home/Article.jsx
+++ src/app/components/home/Article.jsx
@@ -8,7 +8,7 @@ export default function Article() {
         <div className="redback-banner">
                 <img className="home-logo" src={logo}/>
                 <div className="info">
-                    <h3>Welcome to Redback Operations</h3>
+                    <h3>Welcome to Redback Operations Testing Version</h3>
                     <p>Redback Operations builds cutting-edge technologies for connected health, 
                         fitness and sport to enable safe smart exercise. It is focused on providing 
                         a community-driven exercise and well-being gaming experience with competitive elements. </p>
